05cc5cddb30a0f3db91ab2fc758051a6c62462b8
kaylant <kaylant@gmail.com>
2016-06-10 18:53:06 -0400

    displaying content vs downloading content

--- downloadingWebContent/ViewController.swift
+++ downloadingWebContent/ViewController.swift
@@ -10,31 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         let url = NSURL(string: "http://www.stackoverflow.com")!
         
-        _ = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+        // simply displays a website
+        webView.loadRequest(NSURLRequest(URL: url))
         
-            // Will happen when task completes
-            
-            if let urlContent = data {
-                
-                let webContent = NSString()
-                
-                print(urlContent)
-                
-            } else {
-            
-                // Show error message
-                
-            }
-            
-            task?.resume()
+        // the code below downloads a website's content as HTML
         
-        }
+//        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+//        
+//            // Will happen when task completes
+//            
+//            if let urlContent = data {
+//                
+//                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+//                
+//                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                
+//                    self.webView.loadHTMLString(String(webContent!), baseURL: nil)
+//                    
+//                })
+//                
+//            } else {
+//            
+//                // Show error message
+//                
+//            }
+//            
+//        }
+//        
+//        task.resume()
+
         
     }
 
